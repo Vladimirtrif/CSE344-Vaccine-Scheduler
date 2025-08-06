4fec10691d40b45175f206937c592b046b9f56f2
Vladtrif <vova.trifonov@hotmail.com>
2025-08-05 18:04:34 -0700

    Finished Part1?

--- src/main/resources/sqlite/create.sql
+++ src/main/resources/sqlite/create.sql
@@ -11,8 +11,27 @@ CREATE TABLE Availabilities (
     PRIMARY KEY (Time, Username)
 );
 
+CREATE TABLE Patients (
+    Username varchar(255),
+    Salt BINARY(16),
+    Hash BINARY(16),
+    PRIMARY KEY (Username)
+);
+
 CREATE TABLE Vaccines (
     Name varchar(255),
     Doses int,
     PRIMARY KEY (Name)
+);
+
+CREATE TABLE Reservations (
+    ID int,
+    Vaccine varchar(255),
+    Patient varchar(255),
+    Caregiver varchar(255),
+    Time date,
+    PRIMARY KEY (ID),
+    FOREIGN KEY Vaccine REFERENCES Vaccines,
+    FOREIGN KEY Patient REFERENCES Patients,
+    FOREIGN KEY Caregiver REFERENCES Caregivers,
 );
